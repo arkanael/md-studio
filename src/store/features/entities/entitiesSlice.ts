1008306ca3d4f60b95424ff3519e28fe6ae41f08
Luiz Guilherme Bandeira <arkanael@gmail.com>
2026-02-23 13:02:53 -0300

    fix: entitiesSlice.ts - corrige tipos para MDActor/MDTrigger/MDBackground/MDSprite/MDPalette/MDVariable/MDMusic/MDSoundEffect

--- src/store/features/entities/entitiesSlice.ts
+++ src/store/features/entities/entitiesSlice.ts
@@ -1,51 +1,78 @@
 import { createSlice, PayloadAction, createEntityAdapter } from '@reduxjs/toolkit';
-import { Actor, Trigger, Background, Tileset } from '../entities/entitiesTypes';
+import type {
+  MDActor,
+  MDTrigger,
+  MDBackground,
+  MDSprite,
+  MDPalette,
+  MDVariable,
+  MDMusic,
+  MDSoundEffect,
+} from './entitiesTypes';
 import type { RootState } from '../../index';
 
 // -------------------------------------------------
 // EntitiesSlice - Gerencia atores, triggers,
-// backgrounds e tilesets do projeto MD Studio
+// backgrounds, sprites, paletas, variaveis e musicas
+// do projeto MD Studio
+// Usa createEntityAdapter do RTK para CRUD eficiente
 // -------------------------------------------------
 
-const actorsAdapter = createEntityAdapter<Actor>();
-const triggersAdapter = createEntityAdapter<Trigger>();
-const backgroundsAdapter = createEntityAdapter<Background>();
-const tilesetsAdapter = createEntityAdapter<Tileset>();
+const actorsAdapter = createEntityAdapter<MDActor>();
+const triggersAdapter = createEntityAdapter<MDTrigger>();
+const backgroundsAdapter = createEntityAdapter<MDBackground>();
+const spritesAdapter = createEntityAdapter<MDSprite>();
+const palettesAdapter = createEntityAdapter<MDPalette>();
+const variablesAdapter = createEntityAdapter<MDVariable>();
+const musicAdapter = createEntityAdapter<MDMusic>();
+const soundEffectsAdapter = createEntityAdapter<MDSoundEffect>();
 
 interface EntitiesState {
   actors: ReturnType<typeof actorsAdapter.getInitialState>;
   triggers: ReturnType<typeof triggersAdapter.getInitialState>;
   backgrounds: ReturnType<typeof backgroundsAdapter.getInitialState>;
-  tilesets: ReturnType<typeof tilesetsAdapter.getInitialState>;
+  sprites: ReturnType<typeof spritesAdapter.getInitialState>;
+  palettes: ReturnType<typeof palettesAdapter.getInitialState>;
+  variables: ReturnType<typeof variablesAdapter.getInitialState>;
+  music: ReturnType<typeof musicAdapter.getInitialState>;
+  soundEffects: ReturnType<typeof soundEffectsAdapter.getInitialState>;
 }
 
 const initialState: EntitiesState = {
   actors: actorsAdapter.getInitialState(),
   triggers: triggersAdapter.getInitialState(),
   backgrounds: backgroundsAdapter.getInitialState(),
-  tilesets: tilesetsAdapter.getInitialState(),
+  sprites: spritesAdapter.getInitialState(),
+  palettes: palettesAdapter.getInitialState(),
+  variables: variablesAdapter.getInitialState(),
+  music: musicAdapter.getInitialState(),
+  soundEffects: soundEffectsAdapter.getInitialState(),
 };
 
 const entitiesSlice = createSlice({
   name: 'entities',
   initialState,
   reducers: {
+
     // --- Atores ---
-    addActor(state, action: PayloadAction<Actor>) {
+    addActor(state, action: PayloadAction<MDActor>) {
       actorsAdapter.addOne(state.actors, action.payload);
     },
-    updateActor(state, action: PayloadAction<{ id: string; changes: Partial<Actor> }>) {
+    updateActor(state, action: PayloadAction<{ id: string; changes: Partial<MDActor> }>) {
       actorsAdapter.updateOne(state.actors, action.payload);
     },
     removeActor(state, action: PayloadAction<string>) {
       actorsAdapter.removeOne(state.actors, action.payload);
     },
+    setActors(state, action: PayloadAction<MDActor[]>) {
+      actorsAdapter.setAll(state.actors, action.payload);
+    },
 
     // --- Triggers ---
-    addTrigger(state, action: PayloadAction<Trigger>) {
+    addTrigger(state, action: PayloadAction<MDTrigger>) {
       triggersAdapter.addOne(state.triggers, action.payload);
     },
-    updateTrigger(state, action: PayloadAction<{ id: string; changes: Partial<Trigger> }>) {
+    updateTrigger(state, action: PayloadAction<{ id: string; changes: Partial<MDTrigger> }>) {
       triggersAdapter.updateOne(state.triggers, action.payload);
     },
     removeTrigger(state, action: PayloadAction<string>) {
@@ -53,25 +80,66 @@ const entitiesSlice = createSlice({
     },
 
     // --- Backgrounds ---
-    addBackground(state, action: PayloadAction<Background>) {
+    addBackground(state, action: PayloadAction<MDBackground>) {
       backgroundsAdapter.addOne(state.backgrounds, action.payload);
     },
-    updateBackground(state, action: PayloadAction<{ id: string; changes: Partial<Background> }>) {
+    updateBackground(state, action: PayloadAction<{ id: string; changes: Partial<MDBackground> }>) {
       backgroundsAdapter.updateOne(state.backgrounds, action.payload);
     },
     removeBackground(state, action: PayloadAction<string>) {
       backgroundsAdapter.removeOne(state.backgrounds, action.payload);
     },
 
-    // --- Tilesets ---
-    addTileset(state, action: PayloadAction<Tileset>) {
-      tilesetsAdapter.addOne(state.tilesets, action.payload);
+    // --- Sprites ---
+    addSprite(state, action: PayloadAction<MDSprite>) {
+      spritesAdapter.addOne(state.sprites, action.payload);
     },
-    updateTileset(state, action: PayloadAction<{ id: string; changes: Partial<Tileset> }>) {
-      tilesetsAdapter.updateOne(state.tilesets, action.payload);
+    updateSprite(state, action: PayloadAction<{ id: string; changes: Partial<MDSprite> }>) {
+      spritesAdapter.updateOne(state.sprites, action.payload);
     },
-    removeTileset(state, action: PayloadAction<string>) {
-      tilesetsAdapter.removeOne(state.tilesets, action.payload);
+    removeSprite(state, action: PayloadAction<string>) {
+      spritesAdapter.removeOne(state.sprites, action.payload);
+    },
+
+    // --- Paletas ---
+    addPalette(state, action: PayloadAction<MDPalette>) {
+      palettesAdapter.addOne(state.palettes, action.payload);
+    },
+    updatePalette(state, action: PayloadAction<{ id: string; changes: Partial<MDPalette> }>) {
+      palettesAdapter.updateOne(state.palettes, action.payload);
+    },
+    removePalette(state, action: PayloadAction<string>) {
+      palettesAdapter.removeOne(state.palettes, action.payload);
+    },
+
+    // --- Variaveis ---
+    addVariable(state, action: PayloadAction<MDVariable>) {
+      variablesAdapter.addOne(state.variables, action.payload);
+    },
+    updateVariable(state, action: PayloadAction<{ id: string; changes: Partial<MDVariable> }>) {
+      variablesAdapter.updateOne(state.variables, action.payload);
+    },
+    removeVariable(state, action: PayloadAction<string>) {
+      variablesAdapter.removeOne(state.variables, action.payload);
+    },
+
+    // --- Musica ---
+    addMusic(state, action: PayloadAction<MDMusic>) {
+      musicAdapter.addOne(state.music, action.payload);
+    },
+    updateMusic(state, action: PayloadAction<{ id: string; changes: Partial<MDMusic> }>) {
+      musicAdapter.updateOne(state.music, action.payload);
+    },
+    removeMusic(state, action: PayloadAction<string>) {
+      musicAdapter.removeOne(state.music, action.payload);
+    },
+
+    // --- Efeitos Sonoros ---
+    addSoundEffect(state, action: PayloadAction<MDSoundEffect>) {
+      soundEffectsAdapter.addOne(state.soundEffects, action.payload);
+    },
+    removeSoundEffect(state, action: PayloadAction<string>) {
+      soundEffectsAdapter.removeOne(state.soundEffects, action.payload);
     },
 
     // --- Reset tudo ---
@@ -82,17 +150,42 @@ const entitiesSlice = createSlice({
 });
 
 export const {
-  addActor, updateActor, removeActor,
+  addActor, updateActor, removeActor, setActors,
   addTrigger, updateTrigger, removeTrigger,
   addBackground, updateBackground, removeBackground,
-  addTileset, updateTileset, removeTileset,
+  addSprite, updateSprite, removeSprite,
+  addPalette, updatePalette, removePalette,
+  addVariable, updateVariable, removeVariable,
+  addMusic, updateMusic, removeMusic,
+  addSoundEffect, removeSoundEffect,
   resetEntities,
 } = entitiesSlice.actions;
 
 // Selectors
-export const actorSelectors = actorsAdapter.getSelectors((state: RootState) => state.entities.actors);
-export const triggerSelectors = triggersAdapter.getSelectors((state: RootState) => state.entities.triggers);
-export const backgroundSelectors = backgroundsAdapter.getSelectors((state: RootState) => state.entities.backgrounds);
-export const tilesetSelectors = tilesetsAdapter.getSelectors((state: RootState) => state.entities.tilesets);
+export const actorSelectors = actorsAdapter.getSelectors(
+  (state: RootState) => state.entities.actors
+);
+export const triggerSelectors = triggersAdapter.getSelectors(
+  (state: RootState) => state.entities.triggers
+);
+export const backgroundSelectors = backgroundsAdapter.getSelectors(
+  (state: RootState) => state.entities.backgrounds
+);
+export const spriteSelectors = spritesAdapter.getSelectors(
+  (state: RootState) => state.entities.sprites
+);
+export const paletteSelectors = palettesAdapter.getSelectors(
+  (state: RootState) => state.entities.palettes
+);
+export const variableSelectors = variablesAdapter.getSelectors(
+  (state: RootState) => state.entities.variables
+);
+export const musicSelectors = musicAdapter.getSelectors(
+  (state: RootState) => state.entities.music
+);
+export const soundEffectSelectors = soundEffectsAdapter.getSelectors(
+  (state: RootState) => state.entities.soundEffects
+);
 
+export const entitiesReducer = entitiesSlice.reducer;
 export default entitiesSlice.reducer;
