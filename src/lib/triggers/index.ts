4e99ae93baed288cf715904d5d895515a610863a
Luiz Guilherme Bandeira <arkanael@gmail.com>
2026-02-23 15:34:25 -0300

    Create index.ts

---
+++ src/lib/triggers/index.ts
@@ -0,0 +1,6 @@
+// Triggers module - SGDK/Mega Drive adaptation of GB Studio triggers
+// Triggers are rectangular areas that fire scripts when the player walks over them
+export * from './triggerTypes';
+export * from './triggerUtils';
+export { TriggerEditor } from './TriggerEditor';
+export { TriggerSprite } from './TriggerSprite';
