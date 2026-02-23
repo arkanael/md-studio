ea2ba6f33a832363bd6f1b684315a285aefd261f
Luiz Guilherme Bandeira <arkanael@gmail.com>
2026-02-23 15:41:19 -0300

    Create index.ts

---
+++ src/lib/prefabs/index.ts
@@ -0,0 +1,7 @@
+// Prefabs module - SGDK/Mega Drive adaptation of GB Studio prefabs
+// Reusable templates for Actors and Triggers with instance overrides,
+// apply/revert changes, and unpack functionality
+export * from './prefabTypes';
+export * from './prefabUtils';
+export { PrefabEditor } from './PrefabEditor';
+export { PrefabNavigator } from './PrefabNavigator';
