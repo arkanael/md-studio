c46ec22c1ea9489a3ec3149c04eb2b20fc1ad06c
Luiz Guilherme Bandeira <arkanael@gmail.com>
2026-02-23 15:23:43 -0300

    Create index.ts

---
+++ src/lib/actors/index.ts
@@ -0,0 +1,5 @@
+// Actors module - SGDK/Mega Drive adaptation of GB Studio actors
+export * from './actorTypes';
+export * from './actorUtils';
+export { ActorEditor } from './ActorEditor';
+export { ActorSprite } from './ActorSprite';
